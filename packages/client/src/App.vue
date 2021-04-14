5a5b03e70996d85537f25d55e919f94eb9025c8d
jonz94 <jody16888@gmail.com>
2021-04-14 21:15:38 +0800

    ✨ feat(client): add image convertion tool

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <NumberConvertionTool />
+  <ImageConvertionTool />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NumberConvertionTool from './components/NumberConvertionTool.vue';
+import ImageConvertionTool from './components/ImageConvertionTool.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     NumberConvertionTool,
+    ImageConvertionTool,
   },
 });
 </script>
--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { WasmVipsPlugin } from './plugins/wasm-vips';
 import './index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(WasmVipsPlugin, { debug: true });
+app.mount('#app');
